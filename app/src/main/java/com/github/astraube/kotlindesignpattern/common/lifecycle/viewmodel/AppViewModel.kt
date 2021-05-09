6b2a5b8e49d1c93c9dce595caa0638690f5d727f
Andre Straube <a.straube.m@gmail.com>
2021-05-08 23:07:59 -0300

    Hello first commit :)

---
+++ app/src/main/java/com/github/astraube/kotlindesignpattern/common/lifecycle/viewmodel/AppViewModel.kt
@@ -0,0 +1,8 @@
+package com.github.astraube.kotlindesignpattern.common.lifecycle.viewmodel
+
+/**
+ * Created on 08/05/2021
+ * @author André Straube
+ */
+class AppViewModel {
+}
---
+++ app/src/main/java/com/github/astraube/kotlindesignpattern/common/lifecycle/viewmodel/base/BaseViewModel.kt
@@ -0,0 +1,8 @@
+package com.github.astraube.kotlindesignpattern.common.lifecycle.viewmodel.base
+
+/**
+ * Created on 08/05/2021
+ * @author André Straube
+ */
+class BaseViewModel {
+}
