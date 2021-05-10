263501e8b0161c4d2fcbe549191725036fba0419
Andre Straube <a.straube.m@gmail.com>
2021-05-10 14:16:52 -0300

    change home package

--- app/src/main/java/com/github/astraube/kotlindesignpattern/ui/main/MainActivity.kt
+++ app/src/main/java/com/github/astraube/kotlindesignpattern/ui/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.github.astraube.kotlindesignpattern.ui
+package com.github.astraube.kotlindesignpattern.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
